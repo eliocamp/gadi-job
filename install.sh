1e43c0ee0859268840c99dc7f77cbc51a99e3393
Elio Campitelli <eliocampitelli@gmail.com>
2025-08-29 11:57:26 +1000

    Adds messages to install/unisntall

--- install.sh
+++ install.sh
@@ -2,7 +2,8 @@
 set -eu
 
 # Get the directory where the script is located
-SCRIPT_DIR="$(cd "$(dirname "$0")" && pwd)/src"
+SCRIPT_DIR="$(cd "$(dirname "$0")" && pwd)"
+SRC_DIR="$SCRIPT_DIR/src"
 APP_NAME="gadi-job"
 
 # First uninstall so we know we're starting fresh
@@ -11,18 +12,18 @@ bash "$SCRIPT_DIR/uninstall.sh"
 INSTALL_DIR="$HOME/.local/bin/$APP_NAME"
 mkdir -p "$INSTALL_DIR"
 
-echo Copying $SCRIPT_DIR into "$INSTALL_DIR"
-cp -rT "$SCRIPT_DIR" "$INSTALL_DIR"
+echo Copying $SRC_DIR into "$INSTALL_DIR"
+cp -rT "$SRC_DIR" "$INSTALL_DIR"
 
 chmod +x "$INSTALL_DIR/job"
 
-BASHRC="$HOME/.bashrc"
+echo Updating ~/.bashrc
 
+BASHRC="$HOME/.bashrc"
 echo "# BEGIN gadi-job" >> "$BASHRC"
 echo "export PATH=\"$INSTALL_DIR:\$PATH\"" >> "$BASHRC"
 echo "[ -f \"$INSTALL_DIR/completion.sh\" ] && source \"$INSTALL_DIR/completion.sh\"" >> "$BASHRC"
 echo "# END gadi-job" >> "$BASHRC"
-echo "Added $INSTALL_DIR to PATH in $BASHRC"
 
 echo "Installation complete!"
 echo "Please run: source ~/.bashrc or restart your terminal"
--- uninstall.sh
+++ uninstall.sh
@@ -6,7 +6,11 @@ SCRIPT_DIR="$(cd "$(dirname "$0")" && pwd)/src"
 APP_NAME="gadi-job"
 
 INSTALL_DIR="$HOME/.local/bin/$APP_NAME"
+echo "Removing $INSTALL_DIR"
 rm -rf "$INSTALL_DIR"
 
+echo "Removing entrires in .bashrc"
 # Remove everything between markers
 sed -i '/# BEGIN gadi-job/,/# END gadi-job/d' "$HOME/.bashrc" 2>/dev/null || true
+
+echo "Uninstallation complete!"
